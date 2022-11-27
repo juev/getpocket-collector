3f5930d46bec7bf54e12bdca53861a4cb1a3c8a1
Evsyukov Denis <denis@evsyukov.org>
2022-11-27 17:53:16 +0300

    feat: parse README file as database
    closed #3

--- database/database.go
+++ database/database.go
@@ -1,45 +1,118 @@
 package database
 
 import (
-	"encoding/json"
+	"bufio"
+	_ "embed"
+	"fmt"
 	"html"
-	"io"
 	"net/url"
 	"os"
+	"regexp"
 	"strings"
+	"text/template"
+
+	"github.com/juev/getpocket-collector/helpers"
 )
 
 type Title string
 type Url string
-type Database map[Url]Title
+type Date string
+type Database map[Date]map[Url]Title
 
 // ParseFile read data from fileName
 func ParseFile(fileName string) (database Database, err error) {
 	database = Database{}
-	var jsonFile *os.File
-	if jsonFile, err = os.Open(fileName); err != nil {
+	var mdFile *os.File
+	if mdFile, err = os.Open(fileName); err != nil {
 		return nil, err
 	}
-	defer func(jsonFile *os.File) {
-		_ = jsonFile.Close()
-	}(jsonFile)
+	defer func(fileName *os.File) {
+		_ = fileName.Close()
+	}(mdFile)
 
-	byteValue, _ := io.ReadAll(jsonFile)
-	if err = json.Unmarshal(byteValue, &database); err != nil {
+	var re *regexp.Regexp
+	if re, err = regexp.Compile(`### (?P<date>.*)|- \[(?P<title>.*)]\((?P<url>.*)\)`); err != nil {
 		return nil, err
 	}
 
+	fileScanner := bufio.NewScanner(mdFile)
+	fileScanner.Split(bufio.ScanLines)
+	currentDate := Date("")
+	for fileScanner.Scan() {
+		// для даты строка вида: `### {{ $date }}`
+		// для ссылки строка вида: `- [{{ $title }}]({{ $url }})`
+		text := fileScanner.Text()
+		fmt.Println(text)
+		parts := re.FindStringSubmatch(fileScanner.Text())
+		if parts == nil {
+			continue
+		}
+		result := make(map[string]string)
+		for i, name := range re.SubexpNames() {
+			if i != 0 && name != "" {
+				result[name] = parts[i]
+			}
+		}
+		if result == nil {
+			continue
+		}
+		// после обработки в result мы будем иметь мапу с тремя заполненными значениями
+		// map[date: title: url:]
+		if result["date"] != "" {
+			currentDate = Date(result["date"])
+			database[currentDate] = map[Url]Title{}
+			continue
+		}
+		if result["url"] != "" {
+			database[currentDate][Url(result["url"])] = Title(result["title"])
+		}
+	}
 	return database, err
 }
 
 // WriteFile for write data to fileName
-func WriteFile(fileName string, database Database) error {
-	data, err := json.Marshal(database)
-	if err != nil {
+func WriteFile(fileName string, content string, data Database, title string) (err error) {
+	userName, ok := os.LookupEnv("USERNAME")
+	if !ok {
+		// if USERNAME is not setting, we use "juev" by default ;)
+		userName = "juev"
+	}
+
+	var f *os.File
+	if f, err = os.Create(fileName); err != nil {
+		helpers.Exit("cannot create file: %v", err)
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	var funcMap = template.FuncMap{
+		"normalizeUrl":   func(url Url) string { return NormalizeURL(url) },
+		"normalizeTitle": func(title Title) string { return NormalizeTitle(title) },
+	}
+
+	temp := template.Must(template.New("links").Funcs(funcMap).Parse(content))
+
+	r := struct {
+		Title    string
+		UserName string
+		Content  Database
+	}{
+		Title:    title,
+		UserName: userName,
+		Content:  data,
+	}
+
+	var buffer strings.Builder
+	if err = temp.Execute(&buffer, r); err != nil {
 		return err
 	}
 
-	return os.WriteFile(fileName, data, 0660)
+	w := bufio.NewWriter(f)
+	_, _ = w.WriteString(buffer.String())
+	_ = w.Flush()
+
+	return nil
 }
 
 // NormalizeURL should remove all utm from query
--- main.go
+++ main.go
@@ -1,41 +1,30 @@
 package main
 
 import (
-	"bufio"
 	_ "embed"
 	"io"
 	"net/http"
 	"os"
-	"sort"
-	"strings"
-	"text/template"
+	"time"
 
 	"github.com/juev/getpocket-collector/database"
 	"github.com/juev/getpocket-collector/helpers"
 	"github.com/juev/getpocket-collector/rss"
 )
 
-const feedURL = "GETPOCKET_FEED_URL"
-const databaseFolder = "data"
-const databaseFile = "data/database.json"
+const databaseFile = "README.md"
 
 //go:embed templates/template.tmpl
-var content string
+var templateString string
 
 func main() {
 	var err error
-	pocketFeedURL, ok := os.LookupEnv(feedURL)
+	pocketFeedURL, ok := os.LookupEnv("GETPOCKET_FEED_URL")
 	if !ok {
-		helpers.Exit("failed to read %s env variable", feedURL)
+		helpers.Exit("failed to read `GETPOCKET_FEED_URL` env variable")
 	}
 
-	userName, ok := os.LookupEnv("USERNAME")
-	if !ok {
-		userName = "juev"
-	}
-
-	if _, err = os.Stat(databaseFolder); os.IsNotExist(err) {
-		_ = os.Mkdir("data", 0755)
+	if _, err = os.Stat(databaseFile); os.IsNotExist(err) {
 		fileData, _ := os.Create(databaseFile)
 		_ = fileData.Close()
 	}
@@ -58,45 +47,17 @@ func main() {
 		helpers.Exit("cannot parse feed: %v", err)
 	}
 
-	urls := make([]string, 0, len(data))
-	for k := range data {
-		urls = append(urls, string(k))
-	}
-	sort.Strings(urls)
-
-	_ = database.WriteFile(databaseFile, data)
-
-	var f *os.File
-	if f, err = os.Create("README.md"); err != nil {
-		helpers.Exit("cannot create file: %v", err)
-	}
-	defer func(f *os.File) {
-		_ = f.Close()
-	}(f)
-
-	var funcMap = template.FuncMap{
-		"normalizeUrl":   func(url database.Url) string { return database.NormalizeURL(url) },
-		"normalizeTitle": func(title database.Title) string { return database.NormalizeTitle(title) },
+	for _, item := range channel.Item {
+		date, _ := time.Parse(time.RFC1123Z, string(item.PubDate))
+		pubDate := date.Format("02 Jan 2006")
+		if _, ok = data[database.Date(pubDate)]; !ok {
+			data[database.Date(pubDate)] = map[database.Url]database.Title{}
+		}
+		url := database.NormalizeURL(database.Url(item.Link))
+		data[database.Date(pubDate)][database.Url(url)] = database.Title(item.Title)
 	}
 
-	temp := template.Must(template.New("links").Funcs(funcMap).Parse(content))
-
-	r := struct {
-		Title    string
-		UserName string
-		Content  database.Database
-	}{
-		Title:    channel.Title,
-		UserName: userName,
-		Content:  data,
+	if err = database.WriteFile(databaseFile, templateString, data, channel.Title); err != nil {
+		helpers.Exit("failed to write file `%s`: %v", databaseFile, err)
 	}
-
-	var buffer strings.Builder
-	if err = temp.Execute(&buffer, r); err != nil {
-		helpers.Exit("failed to parse template: %v", err)
-	}
-
-	w := bufio.NewWriter(f)
-	_, _ = w.WriteString(buffer.String())
-	_ = w.Flush()
 }
